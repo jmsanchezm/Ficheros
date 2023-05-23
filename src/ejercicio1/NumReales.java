54d7dad2554b5517f4abc2096119c4496b740af9
jmmoreno <jmmoreno@106-15.info2>
2023-05-23 12:50:28 +0200

    Numeros reales. Terminado y comentado

---
+++ src/ejercicio1/NumReales.java
@@ -0,0 +1,52 @@
+package ejercicio1;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class NumReales {
+
+	public static void main(String[] args) {
+		
+		int contador = 0; //Se guardara el contador 
+		int suma = 0; //Se guaradara la suma de los números
+		double media=0; //Se guaradara la media de los numeros
+		
+		//Creamos el Scanner
+		Scanner read = null;
+
+		//Abrimos un bloque try-catch
+		try {
+			
+			// Inicializamos el Scanner con el nombre del fichero que queremos leer
+			read = new Scanner(new FileReader("src\\ejercicio1\\NumerosReale.txt"));
+			
+			read.useLocale(Locale.US);
+			
+			//mientras que siga leyendo caracteres
+			while (read.hasNext()) {
+				//Se irán sumando a la variable suma
+				suma+= read.nextInt();
+				//Iremos contando el número de caracteres
+				contador++;
+			}
+			//Calculamos la media
+			media = suma/contador;
+			
+			//Mostramos los resultado
+			System.out.println(suma);
+			System.out.println(media);
+
+			//Se capturara el siguiente error
+		} catch (FileNotFoundException e) {
+			//Mostramos que ha habido un error
+			System.err.println("No se ha encontrado el fichero");
+			System.err.println(e.getMessage());
+		}
+
+		
+		
+	}
+
+}
